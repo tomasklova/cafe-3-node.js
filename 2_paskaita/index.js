646a5bebe0e174fa9e787106fa1a3a0322eb61b2
tomasklova <tomasklova@gmail.com>
2023-02-11 18:24:24 +0100

    Summit

---
+++ 2_paskaita/index.js
@@ -0,0 +1,14 @@
+const express = required("esxpress");
+consgt app = express();
+const port = 3000;
+
+// req - request - duomenys kuriuos paduoda kviečiantysis pvz. POST user duomenys ar validacijos raktas
+// res -vresponse - duomenys kuriuos grąžinam kai kviečia mūsų API keliu "/"
+// pirmas argumentas - kelias į mūsų API "/"
+
+app.get("/", (req, res)=> {
+   res.send("Hello World!");
+})
+app.listen(port, () => {
+    console.log('Server is running on the ${port} port');
+});
